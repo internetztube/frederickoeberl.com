ec86251735ded6fe1aab8db5e6c8aeeca8a5d00c
Frederic Koeberl <frederic.koeberl@dynatrace.com>
2022-01-07 23:27:18 +0100

    Add linz.at/impftermine.

--- components/projects.vue
+++ components/projects.vue
@@ -2,6 +2,7 @@
   <p>
     <i>Projects? Sure thing!</i><br>
     <a href="https://jaukerl-ooe.m8.at/" target="_blank">jaukerl-ooe.m8.at</a><br>
+    <a href="https://www.linz.at/db/impftermine/" target="_blank">linz.at/impftermine</a><br>
     <a href="https://plugins.craftcms.com/developer/19044" target="_blank">craft cms plugins</a><br>
     <a href="https://github.com/internetztube/internetztube/tree/master/projects/affaere-moertel16-at" target="_blank">moertel16.at</a><br>
     <a href="https://github.com/internetztube/internetztube/blob/master/projects/oe3-statistics/index.md" target="_blank">oe3 statistics</a><br>
