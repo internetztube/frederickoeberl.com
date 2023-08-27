11e4cc72457a11b95261e967397c6b6667abe66f
frederic.koeberl <frederic.koeberl@dynatrace.com>
2023-08-27 17:45:08 +0200

    Remove linz.at/impftermine in favour of jaukerl-ooe.m8.at.

--- components/projects.vue
+++ components/projects.vue
@@ -2,7 +2,6 @@
   <p>
     <i>Projects? Sure thing!</i><br>
     <a href="https://jaukerl-ooe.m8.at/" target="_blank">jaukerl-ooe.m8.at</a><br>
-    <a href="https://www.linz.at/db/impftermine/" target="_blank">linz.at/impftermine</a><br>
     <a href="https://plugins.craftcms.com/developer/frederic.koeberl?craft4" target="_blank">craft cms plugins</a><br>
     <a href="https://github.com/internetztube/internetztube/tree/master/projects/affaere-moertel16-at" target="_blank">moertel16.at</a><br>
     <a href="https://github.com/internetztube/internetztube/blob/master/projects/oe3-statistics/index.md" target="_blank">oe3 statistics</a><br>
