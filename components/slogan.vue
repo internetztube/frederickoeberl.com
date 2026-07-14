86a7a0f4663fc08533738db8aa9880935f25c71e
Frederic Köberl <hello@frederickoeberl.com>
2026-07-14 15:39:15 +0200

    Linz → Vienna

--- components/slogan.vue
+++ components/slogan.vue
@@ -1,5 +1,5 @@
 <template>
   <p>
-    web developer from <a href="https://www.google.com/maps/place/Linz/" target="_blank">Linz, Austria</a>
+    web developer from <a href="https://www.google.com/maps/place/Vienna/" target="_blank">Vienna, Austria</a>
   </p>
 </template>
